3728a24b21a95d5d4b344a3597b57adc3b7ebbf1
Joaquim Pessôa Filho <joaquimp@gmail.com>
2020-06-16 23:09:01 -0300

    Projeto inicial

--- src/main/java/br/pessoa/App.java
+++ src/main/java/br/pessoa/App.java
@@ -3,6 +3,8 @@ package br.pessoa;
 import br.pessoa.resource.ProfessorResource;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 /**
@@ -12,6 +14,11 @@ import io.dropwizard.setup.Environment;
 public class App extends Application<Configuration> {
     public static void main( String[] args ) throws Exception {
         new App().run(new String[]{"server"});
+    }
+
+    @Override
+    public void initialize(final Bootstrap<Configuration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/html", "/site", "index.html"));
     }
 
     @Override
